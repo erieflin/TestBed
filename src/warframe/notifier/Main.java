d14c383d07a7af38da602aae94e0f08e0d3639a6
elliot <elliotrieflin@gmail.com>
2018-07-26 01:04:44 -0400

    put main on timer every 2 min

--- src/warframe/notifier/Main.java
+++ src/warframe/notifier/Main.java
@@ -1,9 +1,23 @@
 package warframe.notifier;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.utils.Time;
+
 public class Main {
+	private static final int interval = 2;
+
 	public static void main(String[] args) {
-		NotificationEngine engine = new NotificationEngine();
-		engine.checkUpdates();
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				NotificationEngine engine = new NotificationEngine();
+				engine.checkUpdates();
+			}
+		}, 0, Time.seconds(60 * interval).getMilliseconds());
 	}
 
 }
